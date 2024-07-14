a86da60cca47b7ca3b58701a84ff70065867aaae
silclementino <167935100+silclementino@users.noreply.github.com>
2024-07-14 19:16:18 -0300

    Update main.py
    Atualização da main para lidar com a exceção de integridade, foi implementar um manipulador global de exceções para capturar IntegrityError e retornar o status code 303 com a mensagem apropriada.

--- workout_api/main.py
+++ workout_api/main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from sqlalchemy.exc import IntegrityError
+from fastapi.responses import JSONResponse
 from workout_api.routers import api_router
 
 app = FastAPI(title='WorkoutApi')
 app.include_router(api_router)
+
+# Manipulador de exceção para IntegrityError
+@app.exception_handler(IntegrityError)
+async def integrity_error_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_303_SEE_OTHER,
+        content={"message": "Já existe um atleta cadastrado com o CPF especificado."}
+    )
